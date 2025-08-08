9097c6109164befa509e801178ffa21eaa29b034
larissadantier <larissa_dantier@hotmail.com>
2025-08-08 19:36:49 -0300

    tests: add tests double in AccountService

--- backend/test/AccountService.test.ts
+++ backend/test/AccountService.test.ts
@@ -1,11 +1,12 @@
+import sinon from "sinon";
 import { AccountDAODatabase, AccountDAOMemory } from "../src/AccountDAO";
 import AccountService from "../src/AccountService";
 
 let accountService: AccountService;
 
 beforeEach(() => { 
-  // const accountDAO = new AccountDAODatabase()
-  const accountDAO = new AccountDAOMemory();
+  const accountDAO = new AccountDAODatabase()
+  // const accountDAO = new AccountDAOMemory();
   accountService = new AccountService(accountDAO);
 })
 test('should be create an account', async () => {  
@@ -68,4 +69,73 @@ test('should be not create an account if the password is invalid', async () => {
   };
 
   await expect(() => accountService.signup(input)).rejects.toThrow(new Error("Invalid password"));
+});
+
+test('should be create an account with stub', async () => {  
+  const saveStub = sinon.stub(AccountDAODatabase.prototype, "save").resolves();
+  const input = {
+    name: "John Doe",
+    email: "john@hotmail.com",
+    document: "87748248800",
+    password: "asdQWE123"
+  };
+
+  const getByIdStub = sinon.stub(AccountDAODatabase.prototype, 'getById').resolves(input)
+
+  const outputSignup = await accountService.signup(input);
+  const outputAccount = await accountService.getAccount(outputSignup.accountId);
+
+  expect(outputSignup.accountId).toBeDefined();
+  expect(outputAccount.name).toBe(input.name);
+  expect(outputAccount.email).toBe(input.email);
+  expect(outputAccount.document).toBe(input.document);
+  expect(outputAccount.password).toBe(input.password);
+
+  saveStub.restore();
+  getByIdStub.restore();
+});
+
+test('should be create an account with spy', async () => {  
+  const saveSpy = sinon.spy(AccountDAODatabase.prototype, 'save');
+  const getByIdSpy = sinon.spy(AccountDAODatabase.prototype, 'getById');
+  const input = {
+    name: "John Doe",
+    email: "john@hotmail.com",
+    document: "87748248800",
+    password: "asdQWE123"
+  };
+
+  const outputSignup = await accountService.signup(input);
+  const outputAccount = await accountService.getAccount(outputSignup.accountId);
+
+  expect(outputSignup.accountId).toBeDefined();
+  expect(outputAccount.name).toBe(input.name);
+  expect(outputAccount.email).toBe(input.email);
+  expect(outputAccount.document).toBe(input.document);
+  expect(outputAccount.password).toBe(input.password);
+  expect(saveSpy.calledOnce).toBe(true);
+  expect(getByIdSpy.calledOnce).toBe(true);
+  expect(getByIdSpy.calledWith(outputSignup.accountId)).toBe(true);
+  saveSpy.restore();
+  getByIdSpy.restore();
+});
+
+test('should be create an account with fake', async () => {  
+  const accountDAO = new AccountDAOMemory();
+  accountService = new AccountService(accountDAO);
+  const input = {
+    name: "John Doe",
+    email: "john@hotmail.com",
+    document: "87748248800",
+    password: "asdQWE123"
+  };
+
+  const outputSignup = await accountService.signup(input);
+  const outputAccount = await accountService.getAccount(outputSignup.accountId);
+
+  expect(outputSignup.accountId).toBeDefined();
+  expect(outputAccount.name).toBe(input.name);
+  expect(outputAccount.email).toBe(input.email);
+  expect(outputAccount.document).toBe(input.document);
+  expect(outputAccount.password).toBe(input.password);
 });
